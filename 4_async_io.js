f60ea1e760eb6fefb6e9f84482864457cfe1dbc9
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-03-19 17:36:06 -0400

    Added first async solution

---
+++ 4_async_io.js
@@ -0,0 +1,18 @@
+// ## MY FIRST ASYNC I/O! (Exercise 4 of 13)
+//
+//   Write a program that uses a single asynchronous filesystem operation to
+//
+//   read a file and print the number of newlines it contains to the console
+//   (stdout), similar to running cat file | wc -l.
+//
+//   The full path to the file to read will be provided as the first
+//   command-line argument.
+
+var fs = require('fs');
+var file = process.argv[2];
+
+fs.readFile(file, 'utf8', function(err, data){
+  if(err) throw err;
+  var length = data.split("\n").length - 1;
+  console.log(length);
+});
