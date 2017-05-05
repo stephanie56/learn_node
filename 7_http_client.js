0740ffff222d62574453f8f04daced6db088974f
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-05-05 12:52:13 -0400

    Completed http client challenge

--- 7_http_client.js
+++ 7_http_client.js
@@ -1,9 +1,17 @@
+// HTTP COLLECT (Exercise 8 of 13)
+//
+//   Write a program that performs an HTTP GET request to a URL provided to you as the first command-line argument. Collect all data from the server (not just the first "data" event) and then write two lines to the console (stdout).
+
 var http = require('http');
 var url = process.argv[2];
 
 http.get(url, function(res){
   res.setEncoding('utf8');
+  var str = [];
   res.on('data', function(data){
-    console.log(data);
+    str.push(data);
+  }).on('end', function(e){
+    var result = str.join('');
+    console.log(result.length + '\n' + result);
   });
 });
